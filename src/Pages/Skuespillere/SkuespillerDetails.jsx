553b73da269d06ad932100255ad8a14967e99d84
Eva-Renata <all2evi@yahoo.com>
2022-09-20 11:35:31 +0200

    skuespillere liste og detaljer

---
+++ src/Pages/Skuespillere/SkuespillerDetails.jsx
@@ -0,0 +1,42 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { NavLink, useParams } from "react-router-dom";
+import { Layout } from "../../App/Layout";
+import styles from "./SkuespillerDetails.module.scss";
+
+export const SkuespillerDetails = () => {
+  const [skuespillerDetails, setSkuespillerDetails] = useState();
+  const { skuespiller_id } = useParams(0);
+
+  useEffect(() => {
+    const getData = async () => {
+      const result = await axios.get(
+        `https://api.mediehuset.net/detutroligeteater/actors/${skuespiller_id}`
+      );
+      setSkuespillerDetails(result.data.item);
+    };
+    getData();
+    //efter kald af funktion den kører kun en gang.
+    //vi skriver i dependency skuespiller_id, så den re-renderer hver gang vi trykker på id.
+  }, [skuespiller_id]);
+
+  return (
+    skuespillerDetails && (
+      <Layout title="Skuespiller details" description="Skuespiller detaljer">
+        <section>
+          <h2>Skuespiller detaljer</h2>
+          <figure className={styles.detailsfigure}>
+            <img src={skuespillerDetails.image} alt="skuespillerDetails" />
+            <figcaption>
+              <h4>{skuespillerDetails.name}</h4>
+              <p>{skuespillerDetails.description}</p>
+            </figcaption>
+          </figure>
+          <NavLink to={"/skuespillere"}>
+            <button className={styles.seallebutton}>ALLE SKUESPILLERE</button>
+          </NavLink>
+        </section>
+      </Layout>
+    )
+  );
+};
--- src/Pages/Skuespillere/Skuespillere.jsx
+++ src/Pages/Skuespillere/Skuespillere.jsx
@@ -1,9 +1,53 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
 import { Layout } from "../../App/Layout";
+import styles from "./Skuespillere.module.scss";
+import { FaUserAlt } from "react-icons/fa";
+import { NavLink } from "react-router-dom";
 
 export const Skuespillere = () => {
+  const [skuespillere, setSkuespillere] = useState([]);
+
+  useEffect(() => {
+    const getData = async () => {
+      const result = await axios.get(
+        "https://api.mediehuset.net/detutroligeteater/actors"
+      );
+      //console.log(result.data.items);
+      const skuespillere = result.data.items;
+      setSkuespillere(skuespillere);
+    };
+    getData();
+  }, []);
+
   return (
-    <Layout title="Skuespillere" description="Skuespillere og detaljer">
-      <h2> Skuespillere</h2>
-    </Layout>
+    skuespillere.length && (
+      <Layout title="Skuespillere" description="Skuespillere og detaljer">
+        <section className={styles.skuespillerewrapper}>
+          <h2> Skuespillere</h2>
+          {skuespillere.map((skuespiller) => {
+            return (
+              <figure key={skuespiller.id}>
+                {/* tjekker om der er billede, hvis der er ikke, så indsætter icon */}
+                {skuespiller.image ? (
+                  <img src={skuespiller.image} alt={skuespiller} />
+                ) : (
+                  <FaUserAlt />
+                )}
+                <figcaption>
+                  <h4>{skuespiller.name}</h4>
+                  <p>{skuespiller.description}</p>
+                </figcaption>
+                <div>
+                  <NavLink to={`/skuespillere/${skuespiller.id}`}>
+                    <button>LÆS MERE</button>
+                  </NavLink>
+                </div>
+              </figure>
+            );
+          })}
+        </section>
+      </Layout>
+    )
   );
 };
