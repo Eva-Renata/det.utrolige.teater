b79e390f5c791548dfb4ddaa744c209e7bdc1dbc
Eva-Renata <all2evi@yahoo.com>
2022-09-23 09:15:43 +0200

    mediaqueries

--- src/Components/Nav/Navigation.jsx
+++ src/Components/Nav/Navigation.jsx
@@ -1,12 +1,16 @@
 import { NavLink } from "react-router-dom";
-import "./Navigation.module.scss";
+import { Twirl as Hamburger } from "hamburger-react";
 import { useAuth } from "../../App/Auth";
+import { useState } from "react";
+import styles from "./Navigation.module.scss";
 
 export const Navigation = () => {
   const { loginData } = useAuth();
+  const [open, setOpen] = useState(false);
+
   return (
     <nav>
-      <ul>
+      <ul className={styles.desktopLinks}>
         <li>
           <NavLink to="/">FORSIDE</NavLink>
         </li>
@@ -24,6 +28,37 @@ export const Navigation = () => {
           )}
         </li>
       </ul>
+
+      <div
+        className={styles.hambrgerwrapper}
+        onClick={() => {
+          setOpen(!open);
+        }}
+      >
+        <Hamburger color="black" className={styles.hamburger} />
+      </div>
+      {open && (
+        <ul className={styles.navlinksopen}>
+          <li>
+            <NavLink to="/">FORSIDE</NavLink>
+          </li>
+          <li>
+            <NavLink to="/forestillingerevents">
+              FORESTILLINGER & EVENTS
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/skuespillere">SKUESPILLERE</NavLink>
+          </li>
+          <li>
+            {loginData ? (
+              <NavLink to="/minside">MIN SIDE</NavLink>
+            ) : (
+              <NavLink to="/login">LOGIN</NavLink>
+            )}
+          </li>
+        </ul>
+      )}
     </nav>
   );
 };
